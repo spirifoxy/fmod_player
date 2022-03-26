2f05df8385bd6fd630fbeedafd4af1dbcc5a5046
spirifoxy <spirifoxy@gmail.com>
2022-03-26 19:12:43 +0100

    Fix FMODPlayerPlugin class

--- android/src/main/kotlin/spirifoxy/com/github/fmod_player/FMODPlayerPlugin.kt
+++ android/src/main/kotlin/spirifoxy/com/github/fmod_player/FMODPlayerPlugin.kt
@@ -4,15 +4,13 @@ import android.app.Activity
 import android.content.Context
 import androidx.annotation.NonNull
 import io.flutter.embedding.engine.plugins.FlutterPlugin
-import io.flutter.embedding.engine.plugins.activity.ActivityAware
-import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
 
 /** FMODPlayerPlugin */
-class FMODPlayerPlugin: FlutterPlugin, MethodCallHandler, ActivityAware {
+class FMODPlayerPlugin: FlutterPlugin, MethodCallHandler {
   /// The MethodChannel that will the communication between Flutter and native Android
   ///
   /// This local reference serves to register the plugin with the Flutter Engine and unregister it
@@ -20,7 +18,6 @@ class FMODPlayerPlugin: FlutterPlugin, MethodCallHandler, ActivityAware {
   private lateinit var channel : MethodChannel
 
   private lateinit var context: Context
-  private lateinit var activity: Activity
 
   override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
     channel = MethodChannel(flutterPluginBinding.binaryMessenger, "fmod_player")
@@ -43,22 +40,6 @@ class FMODPlayerPlugin: FlutterPlugin, MethodCallHandler, ActivityAware {
     org.fmod.FMOD.close()
 
     channel.setMethodCallHandler(null)
-  }
-
-  override fun onDetachedFromActivity() {
-    TODO("Not yet implemented")
-  }
-
-  override fun onReattachedToActivityForConfigChanges(binding: ActivityPluginBinding) {
-    TODO("Not yet implemented")
-  }
-
-  override fun onAttachedToActivity(binding: ActivityPluginBinding) {
-    activity = binding.activity;
-  }
-
-  override fun onDetachedFromActivityForConfigChanges() {
-    TODO("Not yet implemented")
   }
 
   init {
